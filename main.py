9d80f150eacff4d59c8753b53354f33056c2ed83
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-02-08 12:57:00 +0700

    Add exception handler

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
 
 from routers import bulb, party
 
@@ -6,3 +7,11 @@ app = FastAPI()
 
 app.include_router(bulb.router)
 app.include_router(party.router)
+
+
+@app.exception_handler(ValueError)
+async def value_error_handler(request, exc):
+    return JSONResponse(
+        status_code=400,
+        content={"message": str(exc)},
+    )
