22e506d5f7e35e8696e072e1705038e0b2a587a5
Panupun <panupan.ja@ku.th>
2023-02-08 13:10:39 +0700

    party get/put

--- routers/party.py
+++ routers/party.py
@@ -3,13 +3,17 @@ from fastapi import APIRouter
 from models.party import PartyModel
 
 router = APIRouter(prefix="/party")
+party_state: bool = False
 
 
 @router.get("/")
 def get_party():
-    pass
+    return {"party": party_state}
 
 
 @router.put("/")
-def post_party():
-    pass
+def put_party(party: PartyModel):
+    body = party.dict()
+    global party_state
+    party_state = body["party"]
+    return "Update successful"
