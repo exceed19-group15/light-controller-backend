d8af86102a00ecc346bb7aac3512d82b088e31c6
Panupun <panupan.ja@ku.th>
2023-02-08 11:53:31 +0700

    temp

---
+++ core.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+import os
+
+client = MongoClient(f"mongodb://{os.getenv('mongo_username')}:{os.getenv('mongo_password')}@{os.getenv('mongo_host')}:{os.getenv('mongo_port')}")
+
+db = client["exceed15"]
+
+if "light_bulb" not in db.list_collection_names():
+    db.create_collection("light_bulb")
+
+collection = db["light_bulb"]
---
+++ model.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, validator
+
+
+class BulbModel(BaseModel):
+    bulb_id: int
+    room_name: str
+    is_on: bool
+    is_auto: bool
+    light_level: int
+
+    @validator("light_level")
+    def valid_light_level(self, light_level: int):
+        if light_level > 100 or light_level < 0:
+            raise ValueError("Light level can't be above 100 or below 0")
+        return light_level
+
+
+class PartyMode(BaseModel):
+    party: bool
