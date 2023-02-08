3cff14d691b26f2eca9aaa6b3f09480b646e9503
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-02-08 13:06:31 +0700

    Update CORS policy

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
+
+origins = ["*"]
 
 from routers import bulb, party
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(bulb.router)
 app.include_router(party.router)
