ec3a9d94a67927488c803f15ad074c289742e04c
Panupun <panupan.ja@ku.th>
2023-02-08 13:32:30 +0700

    pylint correction

--- routers/bulb.py
+++ routers/bulb.py
@@ -9,15 +9,15 @@ router = APIRouter(prefix="/bulb")
 
 @router.get("/")
 def get_all_bulb() -> List[BulbModel]:
-    return list(collection.find({}, {"_id":0})) 
+    return list(collection.find({}, {"_id": 0}))
 
 
 @router.get("/{bulb_id}")
-def get_bulb(bulb_id :int) -> BulbModel:
-    return collection.find_one({"bulb_id":bulb_id}, {"_id": 0})
+def get_bulb(bulb_id: int) -> BulbModel:
+    return collection.find_one({"bulb_id": bulb_id}, {"_id": 0})
 
 
 @router.put("/{bulb_id}")
-def put_bulb(bulb_id :int , bulb_body: BulbPutModel) -> Dict[str,str]:
-    collection.update_one({"bulb_id":bulb_id},{"$set":bulb_body.dict()})
+def put_bulb(bulb_id: int, bulb_body: BulbPutModel) -> Dict[str, str]:
+    collection.update_one({"bulb_id": bulb_id}, {"$set": bulb_body.dict()})
     return {"message": "Update successful"}
--- routers/party.py
+++ routers/party.py
@@ -1,19 +1,20 @@
 from fastapi import APIRouter
 
 from models.party import PartyModel
+from typing import Dict
 
 router = APIRouter(prefix="/party")
 party_state: bool = False
 
 
 @router.get("/")
-def get_party():
+def get_party() -> Dict[str, bool]:
     return {"party": party_state}
 
 
 @router.put("/")
-def put_party(party: PartyModel):
+def put_party(party: PartyModel) -> Dict[str, str]:
     body = party.dict()
     global party_state
     party_state = body["party"]
-    return "Update successful"
+    return {"message": "Update successful"}
