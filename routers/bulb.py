a2495664acef7b0d99011e976bf08fb50d68c432
Napon <napon.ta@ku.th>
2023-02-08 13:28:26 +0700

    Finish bulb routers

--- routers/bulb.py
+++ routers/bulb.py
@@ -1,21 +1,23 @@
 from fastapi import APIRouter
 
-from core.database import collection, db
 from models.bulb import BulbModel, BulbPutModel
+from core.database import collection
+from typing import List, Dict
 
 router = APIRouter(prefix="/bulb")
 
 
 @router.get("/")
-def get_all_bulb():
-    pass
+def get_all_bulb() -> List[BulbModel]:
+    return list(collection.find({}, {"_id":0})) 
 
 
 @router.get("/{bulb_id}")
-def get_bulb():
-    pass
+def get_bulb(bulb_id :int) -> BulbModel:
+    return collection.find_one({"bulb_id":bulb_id}, {"_id": 0})
 
 
 @router.put("/{bulb_id}")
-def put_bulb():
-    pass
+def put_bulb(bulb_id :int , bulb_body: BulbPutModel) -> Dict[str,str]:
+    collection.update_one({"bulb_id":bulb_id},{"$set":bulb_body.dict()})
+    return {"message": "Update successful"}
